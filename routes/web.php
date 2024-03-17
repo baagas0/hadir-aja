4d1bb3d3bff0c967132dcb577f3bad32b6bf0fb8
baagas0 <baagas0@gmail.com>
2024-03-18 02:22:19 +0700

    asd

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('', function () {
+    dd('route kosong');
+    return redirect()->route('presence-dashboard');
+});
 
 Route::get('/', function () {
     dd('route /');
